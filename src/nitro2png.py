95fa39ba562231ff3f383e2370f95afccbe6c6af
Lande <laande.pro@gmail.com>
2025-08-07 22:24:20 +0200

    add support for reversed bytes & file count as int instead of short

--- src/nitro2png.py
+++ src/nitro2png.py
@@ -3,6 +3,19 @@ import zlib
 from .read_bytes import ReadBytes
 
 
+def is_probably_reversed(data: bytes) -> bool:
+    reversed_snippets = [b'}', b'eman', b'tamrof', b'ezis']
+    png_signature = b'\x89PNG\r\n\x1a\n'
+
+    if all(s in data.lower() for s in reversed_snippets):
+        return True
+
+    if data.startswith(png_signature):
+        return True
+
+    return False
+
+
 def convert(file):
     try:
         with zipfile.ZipFile(file, 'r') as zip_ref:
@@ -15,10 +28,12 @@ def convert(file):
             data = f.read()
 
         read_bytes = ReadBytes(data)
+
         file_count = read_bytes.read_short()
+        if file_count <= 0:
+            file_count = read_bytes.read_short()
 
         for _ in range(file_count):
-            decompressor = None
             name = read_bytes.read_string()
             length = read_bytes.read_int()
             compressed = read_bytes.read_bytes(length)
@@ -26,15 +41,20 @@ def convert(file):
                 decompressor = zlib.decompressobj()
             elif compressed.startswith(b'\x1f\x8b\x08'):
                 decompressor = zlib.decompressobj(16 + zlib.MAX_WBITS)
-
-            if decompressor:
-                try:
-                    decompressed = decompressor.decompress(compressed) + decompressor.flush()
-                    yield name, decompressed
-                except zlib.error as e:
-                    print(f"Cant decompress the file {name}: {e}")
             else:
-                print(f"Format unknown for {name}")
+                print(f"Unkwown compression format for {name}")
+                continue
+
+            try:
+                decompressed = decompressor.decompress(compressed) + decompressor.flush()
+
+                if is_probably_reversed(decompressed[::-1]):
+                    decompressed = decompressed[::-1]
+
+                yield name, decompressed
+
+            except zlib.error as e:
+                print(f"Error decompressing {name}: {e}")
 
 
 def save(file, r):
